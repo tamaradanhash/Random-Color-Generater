bcb558f0cc4ea5b5b7696d542967e01978040d5a
Tamara Danhash <tamaradanhash1989@gmail.com>
2020-02-11 17:07:29 +0100

    Create Cover, Button, Box components and CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Cover from './Cover';
+import Button from './Button';
+import Box from './Box';
+import randomColor from 'randomcolor';
 
 function App() {
+  const [color, setColor] = useState(randomColor());
+  // const color = randomColor;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cover />
+      <Button onClickRandomColor={() => setColor(randomColor())} />
+      <Box color={color} />
     </div>
   );
 }
-
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+export default function Box(props) {
+  return (
+    <button className="Button" onClick={props.onClickRandomColor}>
+      <h1 className="Button-text">Generate Color</h1>
+    </button>
+  );
+}
