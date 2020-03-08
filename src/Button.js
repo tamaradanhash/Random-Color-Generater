e86affceb3313b736bb14340494530826472d286
Tamara Danhash <tamaradanhash1989@gmail.com>
2020-03-08 22:31:58 +0100

    Fix the bug

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Button.css';
-import PropTypes from 'prop-types';
 export default function Box(props) {
   return (
     <button className="Button" onClick={props.onClickRandomColor}>
